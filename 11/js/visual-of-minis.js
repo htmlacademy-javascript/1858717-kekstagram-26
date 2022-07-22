5a0d146054d4e5c3602bbb15ac91bcd5d85f2bf0
Keks <keks@htmlacademy.ru>
2022-07-22 08:29:44 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/visual-of-minis.js
@@ -0,0 +1,18 @@
+const usersPicturesContainer = document.querySelector('.pictures');
+const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const similarListFragment = document.createDocumentFragment();
+
+
+const showUsersPictures = (usersPictures) => {
+  usersPictures.forEach(({url, likes, comments}) => {
+    const pictureElement = picturesTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+    similarListFragment.append(pictureElement);
+  });
+  usersPicturesContainer.append(similarListFragment);
+};
+
+export { showUsersPictures };
+
